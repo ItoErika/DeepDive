ea7ebebbcae9b986bade130f14d882517ea3cf76
Erika Ito <etito@wisc.edu>
2016-06-29 13:59:59 -0500

    Update Example.R

--- Example.R
+++ Example.R
@@ -7,8 +7,64 @@
 library("RPostgreSQL")
 
 # Establish postgresql connection.
+# Remember to start postgres (pgadmin) if it is not already running
 Driver <- dbDriver("PostgreSQL") # Establish database driver
 Connection <- dbConnect(Driver, dbname = "erikaito", host = "localhost", port = 5432, user = "erikaito")
 
 ######################################### Load Data Table Into R ############################################
 DeepDiveData<-dbGetQuery(Connection,"SELECT * FROM pyrite_example_data")
+
+######################################### Useful Code Examples ##############################################
+
+#Find number of documents
+length(unique(DeepDiveData[,"docid"]))
+#Split table into documents
+IndividualDocumentsList<-split(DeepDiveData,DeepDiveData[,"docid"])
+#Check that length of list matches number of documents
+length(IndividualDocumentsList)
+#Select document (ex. 1)
+FirstDocument<-IndividualDocumentsList[[1]]
+#Select first 20 sentences
+FirstDocument[1:20,"words"]
+#Select a sentence (ex. 15th sentence)
+Test<-FirstDocument[15,"words"]
+#get rid of unwanted text or symbols ( ex. }, {, .)
+gsub("\\{|\\}|\\.","",Test)
+#split sentence at commas
+Test<-strsplit(Test,",")
+Test
+#Determine if the word of interest are in the sentence. (ex. pyrite)
+#Remember that Test is a list
+"pyrite"%in%Test[[1]]
+#OR
+"pyrite"%in%unlist(Test)
+#OR if you have multiple words of interest. (ex. pyrite, glauconite, chert, apatite)
+Search<-c("pyrite","glauconite","chert","apatite")
+Search%in%Test[[1]]
+
+
+######################################### Functions for Isolating Text########################################
+IndividualDocumentsList<-split(DeepDiveData,DeepDiveData[,"docid"])
+#Choose Document
+FirstDocument<-IndividualDocumentsList[[1]]
+#Choose Word(s)
+Words<-c("pyrite","glauconite","chert","apatite")
+#Function time
+wordSearch<-function(Sentence,Words) {
+  CleanedSentences<-gsub("\\{|\\}|\\.","",Sentence)
+  SplitSentences<-strsplit(CleanedSentences,",")
+  FoundWords<-Words%in%unlist(SplitSentences)
+  return(FoundWords)
+  }
+  
+# Apply the wordSearch function to all sentences in FirstDocument 
+WordSearchResuls<-sapply(FirstDocument[,"words"],wordSearch,Words)
+#Get sentences where words appear
+which(WordSearchResuls,arr.ind=TRUE) #row number corresponds to Words and column number corresponds to sentences. 
+
+##########
+#Download Names
+source("https://raw.githubusercontent.com/aazaff/paleobiologyDatabase.R/master/communityMatrix.R")
+#Download list of taxonomic names from PBDB
+DataPBDB<-downloadPBDB(Taxa=c("Bivalvia","Brachiopoda"),StartInterval="Cambrian",StopInterval="Holocene")
+
