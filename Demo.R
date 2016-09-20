391f9ae5275a71ce936e7f263d1f74cb264197c4
Erika Ito <etito@wisc.edu>
2016-09-20 11:42:43 -0500

    Create Demo.R

---
+++ Demo.R
@@ -0,0 +1,26 @@
+# Load Libraries
+library("RPostgreSQL")
+
+# Connect to PostgreSQL
+# Load Data Table Into R 
+Driver <- dbDriver("PostgreSQL") # Establish database driver
+Connection <- dbConnect(Driver, dbname = "labuser", host = "localhost", port = 5432, user = "labuser")
+SentencesData<-dbGetQuery(Connection,"SELECT * FROM pyrite_example_data")
+
+# Create a dictionary of words of interest 
+#Select words of interest
+Dictionary<-c("pyrite","glauconite","chert","apatite")
+
+# Write a function to search for words of interest in SentencesData
+
+searchWords<-function(Sentence,Dictionary) {
+    # Split sentences into individual words by separating sentences at each comma, and unlisting them
+    SplitWords<-unlist(strsplit(DocRow["words"],","))
+    # Find Dictionary words which appear in split words of nlp processed sentences
+  	FoundWords<-SplitWords%in%Dictionary
+  	return(FoundWords)
+    }
+
+# Apply function to SentencesData 
+DDResults<-pbapply(SentencesData,1,searchWords,Dictionary)
+
